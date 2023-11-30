45ccc25b71c27694f7128cfd3882c5e675b97a70
Gabrielly Olinsc <gabrielly-04@outlook.com>
2023-11-30 11:13:55 -0300

    shellsort + arquivo dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <fstream>
+#define TMAX 10000
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ifstream fin;
+    int opcao, subopcao,qtdeDados, numComp, numMov; //ordenação decrescente de dados
+    string nomeArquivo, diretorio, caminhoArq;
+    char ch;
+
+    //podemos deixar essa entrada para o usuário inserir onde ele quer colocar o aqrquibo
+    ofstream fout("dados.txt");
+    for(int i=0; i<TMAX; i++){
+        fout<<i+1<<"\n";
+    }
+    fout.close();
+
+    cout<<"Informe o nome do diretorio: ";
+    getline(cin,diretorio);
+    cout<<"Informe o nome do arquivo: ";
+    getline(cin,nomeArquivo);
+    caminhoArq = diretorio + "\" + nomeArquivo;
+    cout<<caminhoArq<<endl;
+    fin.open("dados.txt");  //depois inserir o caminho do arquivo aqui
+    if(!fin)
+        cout << "\nNao posso abrir arquivo copia.xxx\n";
+    else
+    {  //teste para leitura
+         cout << "\nArquivo aberto com sucesso\n";
+        while(fin.get(ch))
+            cout << ch;
+        fin.close();
+    }
     return 0;
 }
--- shellsort.h
+++ shellsort.h
@@ -2,5 +2,41 @@
 #define SHELLSORT_H_INCLUDED
 
 
+/*OBJETIVO: comparar e trocar elementos que estão distantes
+uns dos outros em vez de apenas elementos adjacentes
+envolve dividir a lista em subgrupos menores
+e aplicar o Insertion Sort em cada subgrupo.*/
+
+
+using namespace std;
+
+void shellSort(int vet[], int n, int &c, int &m)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i ++)
+        {
+            int temp = vet[i];
+            int j;
+            for (j = i; j >= gap && vet[j - gap] > temp; j -= gap)
+            {
+
+                vet[j] = vet[j - gap];
+                //acho que aqui seria um conta troca t , talve uma função que faz isso?
+                //conta comparatcao c
+            }
+            vet[j] = temp;
+            //conta troca t
+            //conta comparatcao c
+        }
+    }
+}
+
+void exibeShellSort(int vet[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << "[" << vet[i] << "]";
+    cout << "\n";
+}
 
 #endif // SHELLSORT_H_INCLUDED
